b22253125a11ffacca1da61a78fad5c86ddc2a44
xCryptoBro <xcryptobro@gmail.com>
2025-07-21 10:28:49 -0700

    Reown setup with holding display

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,79 @@
-import Image from "next/image";
+'use client'
+
+import ConnectButton from '@/components/connect-button'
+import Image from 'next/image'
+import { BsPinAngleFill } from 'react-icons/bs'
+import { useReadContract } from 'wagmi'
+import { useAppKitAccount } from '@reown/appkit/react'
+import { pinAbi } from '@/abi/pin'
+
+const pinAddress = '0x0e6dd7ec79912374e4567ed76f8512a8e2343b07'
 
 export default function Home() {
+  const account = useAppKitAccount()
+
+  const { data: balance } = useReadContract({
+    address: pinAddress,
+    abi: pinAbi,
+    functionName: 'balanceOf',
+    args: [account?.address]
+  })
+
+  function formatBalance(balance: bigint) {
+    if (!balance) return '0'
+    try {
+      // 18 decimals for ERC20
+      const decimals = 18
+      const divisor = BigInt('10') ** BigInt(decimals)
+      const whole = balance / divisor
+      return whole.toLocaleString()
+    } catch {
+      return balance.toString()
+    }
+  }
+
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
+    <div className='font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20'>
+      <main className='flex flex-col gap-[32px] row-start-2 items-center sm:items-start'>
+        <div className='flex flex-row items-center gap-2'>
+          <BsPinAngleFill className='text-red-700 text-6xl' />
+          <h1 className='text-6xl sm:text-5xl font-bold tracking-tight'>
+            $PIN
+          </h1>
+        </div>
+        <ol className='font-mono list-inside list-decimal text-sm/6 text-center sm:text-left'>
+          <li className='mb-2 tracking-[-.01em]'>
+            Get your memes, ads or projects seen by thousands.
           </li>
         </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+        <div>
+          <ConnectButton />
+          {typeof balance === 'bigint' && balance > 0 && (
+            <p className='text-sm mt-2'>
+              Your balance:{' '}
+              <span className='font-bold'>{formatBalance(balance)} $PIN</span>
+            </p>
+          )}
         </div>
       </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
+      <footer className='row-start-3 flex gap-[24px] flex-wrap items-center justify-center'>
+        <p>CA: {pinAddress}</p>
         <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+          className='flex items-center gap-2 hover:underline hover:underline-offset-4'
+          href='https://dexscreener.com/base/0x0E6dd7EC79912374E4567ed76F8512A8E2343B07'
+          target='_blank'
+          rel='noopener noreferrer'
         >
           <Image
             aria-hidden
-            src="/file.svg"
-            alt="File icon"
+            src='/globe.svg'
+            alt='Globe icon'
             width={16}
             height={16}
           />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
+          DEX Screener →
         </a>
       </footer>
     </div>
-  );
+  )
 }
---
+++ context/index.tsx
@@ -0,0 +1,56 @@
+'use client'
+
+import React, { type ReactNode } from 'react'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { createAppKit } from '@reown/appkit/react'
+import { base } from '@reown/appkit/networks'
+import { cookieToInitialState, WagmiProvider, type Config } from 'wagmi'
+import { wagmiAdapter, projectId } from '@/config'
+
+const queryClient = new QueryClient()
+
+if (!projectId) {
+  throw new Error('Project ID not defined.')
+}
+
+const metadata = {
+  name: '$PIN',
+  description: 'Get your memes, ads or projects seen by thousands.',
+  url: 'http://localhost:3000',
+  icons: []
+}
+
+export const modal = createAppKit({
+  adapters: [wagmiAdapter],
+  projectId,
+  networks: [base],
+  defaultNetwork: base,
+  metadata: metadata,
+  features: {
+    analytics: true
+  }
+})
+
+function ContextProvider({
+  children,
+  cookies
+}: {
+  children: ReactNode
+  cookies: string | null
+}) {
+  const initialState = cookieToInitialState(
+    wagmiAdapter.wagmiConfig as Config,
+    cookies
+  )
+
+  return (
+    <WagmiProvider
+      config={wagmiAdapter.wagmiConfig as Config}
+      initialState={initialState}
+    >
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    </WagmiProvider>
+  )
+}
+
+export default ContextProvider
